63b8e833585c0d40fcf9bd023ce2a28dff5fd51d
Jonas Marcello <jonas.marcello@esbme.com>
2024-05-16 21:11:01 +0200

    fmt changes in benches

--- benches/genepred.rs
+++ benches/genepred.rs
@@ -1,21 +1,16 @@
-use std::{fs::read_to_string};
+use std::fs::read_to_string;
 
 use atglib::models::TranscriptRead;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-
 use atglib::genepredext;
 
-
 fn read_from_bytes(data: &[u8]) {
-    let transcripts = genepredext::Reader::new(data)
-        .transcripts()
-        .unwrap();
-    assert_eq!(transcripts.len(), 1);    
+    let transcripts = genepredext::Reader::new(data).transcripts().unwrap();
+    assert_eq!(transcripts.len(), 1);
 }
 
 fn read_genepred_bench(c: &mut Criterion) {
-
     c.bench_function("NM_001365057.2", |b| {
         let data = read_to_string("tests/data/NM_001365057.2.genepredext").unwrap();
         b.iter(|| read_from_bytes(black_box(data.as_bytes())))
@@ -37,8 +32,5 @@ fn read_genepred_bench(c: &mut Criterion) {
     });
 }
 
-criterion_group!(
-    genepred,
-    read_genepred_bench,
-);
+criterion_group!(genepred, read_genepred_bench,);
 criterion_main!(genepred);
